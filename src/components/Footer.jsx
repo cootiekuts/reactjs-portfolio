b5c1fc887538039fbf16df18e01270c1fb8f33ae
cootiekuts <kutswebhosting@gmail.com>
2024-05-08 12:28:50 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     return (
       <div className="footer-div">
       <footer>
-        <p><img src="img/ooga.png" alt="oogabooga" /> {new Date().getFullYear()} Portfolio</p>
+        <p><img src="./img/ooga.png" alt="oogabooga" /> {new Date().getFullYear()} Portfolio</p>
       </footer>
       </div>
     );
