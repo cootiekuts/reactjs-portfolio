42900fb552719a264229cca906267114e55a77dd
cootiekuts <kutswebhosting@gmail.com>
2024-05-08 12:48:17 +0800

    update sht

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ function Footer() {
     return (
       <div className="footer-div">
       <footer>
-        <p><img src="/images/ooga.png" alt="oogabooga" /> {new Date().getFullYear()} Portfolio</p>
+        <p><img src="src/components/images/ooga.png"></img>{new Date().getFullYear()} Portfolio</p>
       </footer>
       </div>
     );
   }
   
   export default Footer
-  S
+  
