91a673f92ca61ffed1c62063ec199d2a3de424fe
cootiekuts <kutswebhosting@gmail.com>
2024-05-17 17:20:50 +0800

    responsive update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -84,6 +84,7 @@ export default function Content() {
       rect.right <= (window.innerWidth || html.clientWidth)
     );
   }
+  
   return (
     
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 
 function Header() {
     return(
-        <div className="header-div">
+
             <header>            
-                <h1><img className='prof-img' src='img/ooga.png'/>OOGA BOOGA<img className='prof-img' src='img/ooga.png'/></h1>
+                <h1><img src='img/ooga.png'/>OOGA BOOGA<img src='img/ooga.png'/></h1>
             </header>
-        </div>
     );
 
 }
