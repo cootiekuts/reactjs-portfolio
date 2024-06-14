2eeb917c04ca1bb8aa28aba19101b11178a65f5a
cootiekuts <kutswebhosting@gmail.com>
2024-06-14 20:00:22 +0800

    basic info animation update

--- src/components/BasicProfile.jsx
+++ src/components/BasicProfile.jsx
@@ -12,23 +12,16 @@ export function BasicProfile(props) {
 
     <div className="top-info-div">
         <img className={imgClName} src={profImglink}/>
-        <div className="top-content-div">
             <span className={nameClName}><h3>Name: {name}</h3></span>
-            <address className={addClName}>
-            <span> <h3>Mobile Number: {mobNum}</h3></span>
-                <span><h3>Email Address: {emailAdd}
-                    <a href={emailLink} target='blank'><img className={iconClName} src={emailIcon}/></a></h3>
-                </span>
-                <div className={contactClName}>
-                    <h3>Socials: 
-                        <a href={fbLink}  target="_blank"><img className={iconClName} src={fbIconLink}/></a>
-                        <a href={lILink}  target="_blank"><img className={iconClName} src={liIconLink}/></a>
-                    </h3>
-            </div>
-        </address>
-        <span><h3>Resume PDF:
-        <a href={resumeLink}  target="_blank"><img className={iconClName} src={gDIconLink}/></a></h3></span>
-        </div>
+            <span className="mobile-number-span"><h3>Mobile Number: {mobNum}</h3></span>
+            <span className="email-span">
+                <h3>Email Address: {emailAdd}
+                <a href={emailLink} target='blank'><img className={iconClName} src={emailIcon}/></a></h3>
+            </span>
+        <span className="resume-span">
+            <h3>Resume PDF:<a href={resumeLink}  target="_blank"><img className={iconClName} src={gDIconLink}/></a></h3>
+        </span>
+
     </div>
   )
 }
--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -3,17 +3,15 @@ import {Image} from './Image'
 export function SkillBar(label) {
   return (
     <div key={label.key}>
-      <div style={{display: "flex", flexFlow:"column"}}>
+      <span className='skill-description-span'>
         <h3>{label.label}</h3>
-        <Image link ={label.link} imgWidth={label.imgWidth} imgHeight={label.imgHeight} shadow={label.shadow}/>
-        
-      </div>
-      <div className='skill-bar-div' 
-      style={{display: "flex", backgroundColor: "hsl(148, 100%, 10%)", width: "30vw", borderRadius: "50px", boxShadow: "1px 1px 2.1vh black", border: "2px solid white", placeItems:"center"}}>
-          <div className='skill-rate-div' style={{display: "flex", backgroundColor: "hsl(148, 100%, 45%)", width: label.width, height: "max", borderRadius: "50px", justifyContent:"right"}}>  
+        <Image link ={label.link} imgWidth={label.imgWidth} imgHeight={label.imgHeight} shadow={label.shadow}/> 
+      </span>
+      <span className='skill-bar-span'>
+          <span className='skill-rate-span' style={{width: label.width}}>  
             <h3> {label.width}</h3>
-          </div>
-      </div>
+          </span>
+      </span>
     </div>
   )
 }
